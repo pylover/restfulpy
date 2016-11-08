b92befc8f685c6032a12418d0511d2fdf1d3194a
Vahid <vahid.mardani@gmail.com>
2016-11-08 21:00:40 +0330

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+import re
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
+
+# reading package version (without reloading it)
+with open(join(dirname(__file__), 'restfulpy', '__init__.py')) as v_file:
+    package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+
+dependencies = [
+    'nanohttp',
+    'argcomplete',
+    'alembic',
+    'PyJWT',
+    'appdirs',
+    'sqlalchemy',
+    'webtest',
+    'nose',
+]
+
+
+setup(
+    name="restfulpy",
+    version=package_version,
+    author="Vahid Mardani",
+    author_email="vahid.mardani@gmail.com",
+    install_requires=dependencies,
+    packages=find_packages(),
+    test_suite="restfulpy.tests",
+)
