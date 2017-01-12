a8ce607ac34aef81c8105526e503914da152092c
Vahid <vahid.mardani@gmail.com>
2017-01-13 00:37:34 +0330

    Parsing pagination parameters safely.

--- restfulpy/orm/mixines.py
+++ restfulpy/orm/mixines.py
@@ -116,8 +116,15 @@ class PaginationMixin:
         # noinspection PyUnresolvedReferences
         query = query or cls.query
 
-        take = int(context.query_string.get('take') or context.environ.get(cls.__take_header_key__) or cls.__max_take__)
-        skip = int(context.query_string.get('skip') or context.environ.get(cls.__skip_header_key__) or 0)
+        try:
+            take = int(context.query_string.get('take') or context.environ.get(cls.__take_header_key__) or cls.__max_take__)
+        except ValueError:
+            take = cls.__max_take__
+
+        try:
+            skip = int(context.query_string.get('skip') or context.environ.get(cls.__skip_header_key__) or 0)
+        except ValueError:
+            skip = 0
 
         if take > cls.__max_take__:
             raise HttpBadRequest()
