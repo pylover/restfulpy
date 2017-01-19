bdbefcfd5b9c4efd72839d71b5d81b1e1184a938
Vahid <vahid.mardani@gmail.com>
2017-01-19 19:34:35 +0330

    Verify custom verb

--- restfulpy/testing/documentation.py
+++ restfulpy/testing/documentation.py
@@ -235,7 +235,8 @@ class DocumentaryTestApp(TestApp):
         resp = getattr(self, method.lower())(real_url, **kwargs)
 
         if doc:
-            self.document(role, method, url, resp, kwargs.get('headers'), model=model, params=params, query_string=query_string)
+            self.document(role, method, url, resp, kwargs.get('headers'), model=model, params=params,
+                          query_string=query_string)
         return resp
 
     def metadata(self, url, params='', headers=None, extra_environ=None,
@@ -256,6 +257,18 @@ class DocumentaryTestApp(TestApp):
         if xhr:
             headers = self._add_xhr_header(headers)
         return self._gen_request('UNDELETE', url, params=params, headers=headers,
+                                 extra_environ=extra_environ, status=status,
+                                 upload_files=upload_files,
+                                 expect_errors=expect_errors,
+                                 content_type=content_type,
+                                 )
+
+    def verify(self, url, params='', headers=None, extra_environ=None,
+               status=None, upload_files=None, expect_errors=False,
+               content_type=None, xhr=False):
+        if xhr:
+            headers = self._add_xhr_header(headers)
+        return self._gen_request('VERIFY', url, params=params, headers=headers,
                                  extra_environ=extra_environ, status=status,
                                  upload_files=upload_files,
                                  expect_errors=expect_errors,
