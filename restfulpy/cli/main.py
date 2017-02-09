3938af86f7dc0da7111ffb8699780f7b7f16e8d3
Vahid <vahid.mardani@gmail.com>
2017-02-10 02:43:31 +0330

    Nothing

--- restfulpy/cli/main.py
+++ restfulpy/cli/main.py
@@ -16,7 +16,7 @@ class MainLauncher(Launcher):
     def __init__(self, application):
         self.application = application
         self.parser = parser = argparse.ArgumentParser(description='%s command line interface.' % application.name)
-        parser.add_argument('-p', '--process-name', metavar="PREFIX", default=application.name, nargs=1,
+        parser.add_argument('-p', '--process-name', metavar="PREFIX", default=application.name,
                             help='A string indicates the logger prefix for this process, it helps to configure '
                                  'separate log files per process.')
         parser.add_argument('-c', '--config-file', metavar="FILE",
