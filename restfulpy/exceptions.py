16a7ceeb264716bd80ae8690efd798e65d7de7a6
pylover <vahid@carrene.com>
2017-03-13 20:07:40 +0330

    v0.2.16

--- restfulpy/exceptions.py
+++ restfulpy/exceptions.py
@@ -6,16 +6,3 @@ class RestfulException(Exception):
             'type': str(type(self)),
             'content': str(self),
         }
-
-
-class ValidationError(RestfulException):
-    field = None
-
-    def __init__(self, field_name, *args, **kw):
-        self.field = field_name
-        super(ValidationError, self).__init__(*args, **kw)
-
-    def to_json(self):
-        d = super(ValidationError, self).to_json()
-        d['field'] = self.field
-        return d
