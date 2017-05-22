85dbe1763b91fb301e28bceeb1dcee05684f6422
pylover <vahid.mardani@gmail.com>
2017-05-22 11:55:06 +0430

    v0.7.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(join(dirname(__file__), 'restfulpy', '__init__.py')) as v_file:
 
 
 dependencies = [
-    'nanohttp>=0.7.1',
+    'nanohttp>=0.7.2',
     'argcomplete',
     'ujson',
     'appdirs',
