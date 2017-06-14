5adc5ae6fd59ddb5e688c16206a09d636f139fd6
pylover <vahid@carrene.com>
2017-06-14 11:28:56 +0430

    Introducing the `UnsupportedError`

--- restfulpy/exceptions.py
+++ restfulpy/exceptions.py
@@ -6,3 +6,12 @@ class RestfulException(Exception):
             'type': str(type(self)),
             'content': str(self),
         }
+
+
+class UnsupportedError(Exception):
+    """Used to indicate we're not supporting a feature and or method.
+
+    .. versionadded:: 0.11.0
+
+    """
+    pass
