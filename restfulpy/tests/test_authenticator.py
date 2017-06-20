dcf8feb3bec9e8e57755ae615b3b611c0d2df1c4
pylover <vahid.mardani@gmail.com>
2017-06-20 12:19:22 +0430

    Test refresh token

--- restfulpy/tests/test_authenticator.py
+++ restfulpy/tests/test_authenticator.py
@@ -1,12 +1,14 @@
 
 
 import unittest
+import time
 
-from nanohttp import json, Controller, context
+from nanohttp import json, Controller, context, settings
 
 from restfulpy.authentication import Authenticator
+from restfulpy.authorization import authorize
 from restfulpy.principal import JwtPrincipal, JwtRefreshToken
-from restfulpy.testing import WebAppTestCase
+from restfulpy.testing import WebAppTestCase, As
 from restfulpy.tests.helpers import MockupApplication
 
 
@@ -28,7 +30,7 @@ class MockupStatelessAuthenticator(Authenticator):
         ))
 
     def create_principal(self, member_id=None, session_id=None):
-        return JwtPrincipal(dict(id=1, email='test@example.com', roles=['admin', 'test']))
+        return JwtPrincipal(dict(id=1, email='test@example.com', roles=['admin', 'test'], sessionId='1'))
 
 
 class Root(Controller):
@@ -42,9 +44,24 @@ class Root(Controller):
         principal = context.application.__authenticator__.login((context.form['email'], context.form['password']))
         return dict(token=principal.dump())
 
+    @json
+    @authorize
+    def me(self):
+        return context.identity.payload
+
 
 class AuthenticatorTestCase(WebAppTestCase):
     application = MockupApplication('MockupApplication', Root(), authenticator=MockupStatelessAuthenticator())
+
+    @classmethod
+    def configure_app(cls):
+        cls.application.configure(force=True)
+        settings.merge("""
+            jwt:
+              max_age: .3
+              refresh_token:
+                max_age: 3
+        """)
 
     def test_login(self):
         response, headers = self.request('ALL', 'GET', '/login', json=dict(email='test@example.com', password='test'))
@@ -54,6 +71,39 @@ class AuthenticatorTestCase(WebAppTestCase):
         self.wsgi_app.jwt_token = response['token']
         response, headers = self.request('ALL', 'GET', '/', json=dict(a='a', b='b'))
         self.assertEqual(headers['X-Identity'], '1')
+
+    def test_refresh_token(self):
+        response, headers = self.request('ALL', 'GET', '/login', json=dict(email='test@example.com', password='test'))
+        refresh_token = headers['Set-Cookie'].split('; ')[0]
+        self.assertIn('token', response)
+        self.assertTrue(refresh_token.startswith('refresh-token='))
+
+        # Login on client
+        token = response['token']
+        self.wsgi_app.jwt_token = token
+
+        # Request a protected resource after the token has been expired expired, without the cookies
+        time.sleep(1)
+        self.request(As.member, 'GET', '/me', expected_status=401)
+
+        # Request a protected resource after the token has been expired expired, with appropriate cookies.
+        response, response_headers = self.request(
+            As.member, 'GET', '/me',
+            headers={
+                'Cookie': refresh_token
+            }
+        )
+        self.assertIn('X-New-JWT-Token', response_headers)
+        self.assertIsNotNone(response_headers['X-New-JWT-Token'])
+
+        # Test with invalid Refresh Token
+        self.request(
+            As.member, 'GET', '/me',
+            headers={
+                'Cookie': 'refresh-token=InvalidToken'
+            },
+            expected_status=400
+        )
 
 
 if __name__ == '__main__':
