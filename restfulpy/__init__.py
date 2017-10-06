2733debd39709868559da211bbbd88ca0656cbe6
pylover <vahid.mardani@gmail.com>
2017-10-06 03:59:15 +0330

    v0.33.2

--- restfulpy/__init__.py
+++ restfulpy/__init__.py
@@ -12,7 +12,7 @@ from .cli import Launcher
 warnings.filterwarnings('ignore', message='Unknown REQUEST_METHOD')
 
 
-__version__ = '0.33.1'
+__version__ = '0.33.2'
 
 
 class RestfulpyMainLauncher(Launcher):
