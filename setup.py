b45fd93741db874845c0fd68ffb9b1b20d5da821
pylover <vahid.mardani@gmail.com>
2017-12-16 20:28:44 +0330

    Migrating to nanohttp >= 0.22.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(join(dirname(__file__), 'restfulpy', '__init__.py')) as v_file:
 
 
 dependencies = [
-    'nanohttp >= 0.20.1',
+    'nanohttp >= 0.22.0',
     'argcomplete',
     'ujson',
     'appdirs',
