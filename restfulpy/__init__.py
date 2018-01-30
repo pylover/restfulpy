8a239364dae5e8ec359344c1e6f8774769a10265
pylover <vahid@carrene.com>
2018-01-30 12:45:17 +0330

    v0.41.3, rebasing with master

--- restfulpy/__init__.py
+++ restfulpy/__init__.py
@@ -12,7 +12,7 @@ from .cli import Launcher
 warnings.filterwarnings('ignore', message='Unknown REQUEST_METHOD')
 
 
-__version__ = '0.41.2'
+__version__ = '0.41.3'
 
 
 class RestfulpyMainLauncher(Launcher):
