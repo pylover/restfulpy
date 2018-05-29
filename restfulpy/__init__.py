d83a5e28afa0f3ae1d0473388369aa2b73aed81c
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-05-29 14:44:56 +0430

    v0.49.3

--- restfulpy/__init__.py
+++ restfulpy/__init__.py
@@ -12,7 +12,7 @@ from .cli import Launcher
 warnings.filterwarnings('ignore', message='Unknown REQUEST_METHOD')
 
 
-__version__ = '0.49.2'
+__version__ = '0.49.3'
 
 
 class RestfulpyMainLauncher(Launcher):
