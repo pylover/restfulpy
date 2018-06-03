3e3c530bc0bbac5a33259db2890f40d3007df9d7
shayan <email@example.com>
2018-06-03 14:05:58 +0430

    Move mockupApplication class from tests/helpers.py to testing/helpers.py

--- restfulpy/testing/helpers.py
+++ restfulpy/testing/helpers.py
@@ -3,7 +3,28 @@ import ujson
 
 from restfulpy.messaging import Messenger
 from restfulpy.principal import JwtPrincipal
+from restfulpy.application import Application
 
+
+# noinspection PyAbstractClass
+class MockupApplication(Application):
+    builtin_configuration = '''
+    db:
+      test_url: postgresql://postgres:postgres@localhost/restfulpy_test
+      administrative_url: postgresql://postgres:postgres@localhost/postgres
+    logging:
+      loggers:
+        default:
+          level: critical
+    '''
+
+    def configure(self, files=None, context=None, **kwargs):
+        _context = dict(
+            process_name='restfulpy_unittests'
+        )
+        if context:
+            _context.update(context)
+        super().configure(files=files, context=_context, **kwargs)
 
 class MockupMessenger(Messenger):
     _last_message = None
