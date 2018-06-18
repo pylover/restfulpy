22617d1a6bcf6f724f363b5ec9ee2c99aee54388
Arashfatahzade <fatahzade@gmail.com>
2018-06-18 15:34:21 +0430

    Migrate to nanohttp v0.29.0

--- restfulpy/__init__.py
+++ restfulpy/__init__.py
@@ -12,7 +12,7 @@ from .cli import Launcher
 warnings.filterwarnings('ignore', message='Unknown REQUEST_METHOD')
 
 
-__version__ = '0.49.4'
+__version__ = '0.50.0'
 
 
 class RestfulpyMainLauncher(Launcher):
