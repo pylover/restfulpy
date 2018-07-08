34265dd21a069c5fa8cff7f551e52fc7bba536c1
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-07-08 13:06:49 +0430

    Improve coverage, #182

--- restfulpy/tests/helpers.py
+++ restfulpy/tests/helpers.py
@@ -1,12 +1,9 @@
 
-import asyncio
 import functools
 import re
 import sys
 import unittest
-from os.path import join, abspath
-from unittest.case import _Outcome
-from unittest.util import safe_repr
+from os.path import join
 from urllib.parse import quote, urlencode
 
 import ujson
@@ -152,9 +149,9 @@ class WebAppTestCase(unittest.TestCase):
         cls.drop_database()
         super().tearDownClass()
 
-    def _print_statuses_mismatch_error(self, expected_status, response):
+    def _print_statuses_mismatch_error(self, expected_status, response):  # pragma: no cover
 
-        if isinstance(expected_status,int) and response.status_code != expected_status:
+        if isinstance(expected_status, int) and response.status_code != expected_status:
 
             print_ = functools.partial(
                 print,
@@ -216,135 +213,3 @@ class WebAppTestCase(unittest.TestCase):
             result = response.body
 
         return result, response.headers
-
-    def assertDictContainsSubset(self, dictionary, subset, msg=None):
-        """Checks whether dictionary is a superset of subset."""
-
-        missing = []
-        mismatched = []
-        for key, value in subset.items():
-            if key not in dictionary:
-                missing.append(key)
-            elif value != dictionary[key]:
-                mismatched.append(
-                    '%s, expected: %s, actual: %s' %
-                    (safe_repr(key), safe_repr(value),
-                    safe_repr(dictionary[key]))
-                )
-
-        if not (missing or mismatched):
-            return
-
-        standard_message = ''
-        if missing:
-            standard_message = 'Missing: %s' % ','.join(
-                safe_repr(m) for m in missing)
-        if mismatched:
-            if standard_message:
-                standard_message += '; '
-            standard_message += 'Mismatched values: %s' % ','.join(mismatched)
-
-        self.fail(self._formatMessage(msg, standard_message))
-
-
-class ModelRestCrudTestCase(WebAppTestCase):
-    __model__ = None
-
-    def request(self, *args, model=None, **kwargs):
-        return super(ModelRestCrudTestCase, self).request(
-            *args,
-            model=self.__model__,
-            **kwargs
-        )
-
-
-class AioTestCase(unittest.TestCase):
-    # noinspection PyPep8Naming
-    def __init__(self, *args, loop=None):
-        self.loop = loop or asyncio.get_event_loop()
-        super(AioTestCase, self).__init__(*args)
-
-    def _run_method(self, func, *args, **kwargs):
-        if asyncio.iscoroutinefunction(func):
-            return self.loop.run_until_complete(func(*args, **kwargs))
-        else:
-            return func(*args, **kwargs)
-
-    def run(self, result=None):
-        orig_result = result
-        if result is None:
-            result = self.defaultTestResult()
-            start_test_run = getattr(result, 'startTestRun', None)
-            if start_test_run is not None:
-                start_test_run()
-
-        result.startTest(self)
-
-        test_method = getattr(self, self._testMethodName)
-        if getattr(self.__class__, "__unittest_skip__", False) or getattr(
-                test_method, "__unittest_skip__", False):
-            # If the class or method was skipped.
-            try:
-                skip_why = (getattr(self.__class__, '__unittest_skip_why__','')
-                     or getattr(test_method, '__unittest_skip_why__',''))
-                self._addSkip(result, self, skip_why)
-            finally:
-                result.stopTest(self)
-            return
-        expecting_failure_method = getattr(test_method, "__unittest_expecting_failure__", False)
-        expecting_failure_class = getattr(self, "__unittest_expecting_failure__", False)
-        expecting_failure = expecting_failure_class or expecting_failure_method
-        outcome = _Outcome(result)
-        try:
-            self._outcome = outcome
-
-            with outcome.testPartExecutor(self):
-                self._run_method(self.setUp)
-            if outcome.success:
-                outcome.expecting_failure = expecting_failure
-                with outcome.testPartExecutor(self, isTest=True):
-                    self._run_method(test_method)
-                outcome.expecting_failure = False
-                with outcome.testPartExecutor(self):
-                    self._run_method(self.tearDown)
-
-            self.doCleanups()
-            for test, reason in outcome.skipped:
-                self._addSkip(result, test, reason)
-            self._feedErrorsToResult(result, outcome.errors)
-            if outcome.success:
-                if expecting_failure:
-                    if outcome.expectedFailure:
-                        self._addExpectedFailure(result, outcome.expectedFailure)
-                    else:
-                        self._addUnexpectedSuccess(result)
-                else:
-                    result.addSuccess(self)
-            return result
-        finally:
-            result.stopTest(self)
-            if orig_result is None:
-                stop_test_run = getattr(result, 'stopTestRun', None)
-                if stop_test_run is not None:
-                    stop_test_run()
-
-            # explicitly break reference cycles:
-            # outcome.errors -> frame -> outcome -> outcome.errors
-            # outcome.expectedFailure -> frame -> outcome -> outcome.expectedFailure
-            outcome.errors.clear()
-            outcome.expectedFailure = None
-
-            # clear the outcome, no more needed
-            self._outcome = None
-
-    # noinspection PyMethodOverriding
-    async def assertRaises(self, expected_exception, func, *args, **kwargs):
-        try:
-            await func(*args, **kwargs)
-        except Exception as ex:
-            if not isinstance(ex, expected_exception):
-                # noinspection PyUnresolvedReferences
-                msg = self._formatMessage(self.msg, "%s not raised by %s" % (
-                expected_exception, func.__name__))
-                raise self.failureException(msg)
-
--- restfulpy/tests/test_refreshtoken_without_ssl.py
+++ restfulpy/tests/test_refreshtoken_without_ssl.py
@@ -1,57 +1,21 @@
 import time
 import unittest
-from nanohttp import json, Controller, context, settings
+from nanohttp import Controller, settings
 from restfulpy.authentication import StatefulAuthenticator
-from restfulpy.authorization import authorize
 from restfulpy.principal import JwtPrincipal, JwtRefreshToken
 from restfulpy.tests.helpers import WebAppTestCase
 from restfulpy.testing import MockupApplication
-
-
-class MockupMember:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
 
 
 roles = ['admin', 'test']
 
 
 class MockupAuthenticator(StatefulAuthenticator):
-    def validate_credentials(self, credentials):
-        email, password = credentials
-        if password == 'test':
-            return MockupMember(id=1, email=email, roles=['admin', 'test'])
-
-    def create_refresh_principal(self, member_id=None):
-        return JwtRefreshToken(dict(
-            id=member_id
-        ))
-
-    def create_principal(self, member_id=None, session_id=None):
-        return JwtPrincipal(dict(id=1, email='test@example.com', roles=roles, sessionId='1'))
+    pass
 
 
 class Root(Controller):
-
-    @json
-    def login(self):
-        principal = context.application.__authenticator__.login(
-            context.form['email'], context.form['password']
-        )
-
-        return dict(token=principal.dump())
-
-
-    @json
-    @authorize
-    def me(self):
-        return context.identity.payload
-
-    @json
-    @authorize
-    def invalidate_token(self):
-        context.application.__authenticator__.invalidate_member(1)
-        return context.identity.payload
+    pass
 
 
 class StatefulAuthenticatorTestCase(WebAppTestCase):
