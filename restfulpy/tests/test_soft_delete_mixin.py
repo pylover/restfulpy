26aa41d676612be76a7fb7b5bb3e09cb3000e52a
mohammad <mohammadsheikhian70@gmail.com>
2018-07-15 16:32:29 +0430

    Passing the soft delete mixin test case, #191

---
+++ restfulpy/tests/test_soft_delete_mixin.py
@@ -0,0 +1,51 @@
+import pytest
+
+from sqlalchemy import Unicode
+from nanohttp import HTTPConflict
+
+from restfulpy.orm import DeclarativeBase, Field, SoftDeleteMixin
+
+
+class SoftDeleteCheckingModel(SoftDeleteMixin, DeclarativeBase):
+    __tablename__ = 'soft_delete_checking_model'
+
+    title = Field(Unicode(50), primary_key=True)
+
+
+def test_soft_delete_mixin(db):
+    session = db()
+
+    # noinspection PyArgumentList
+    instance = SoftDeleteCheckingModel(
+        title='test title'
+    )
+    session.add(instance)
+    session.commit()
+    instance.assert_is_not_deleted()
+    with pytest.raises(ValueError):
+        instance.assert_is_deleted()
+
+    instance = session.query(SoftDeleteCheckingModel).one()
+    instance.soft_delete()
+    session.commit()
+    instance.assert_is_deleted()
+    with pytest.raises(ValueError):
+        instance.assert_is_not_deleted()
+
+    query = session.query(SoftDeleteCheckingModel)
+    assert SoftDeleteCheckingModel.filter_deleted(query).count() == 1
+    assert SoftDeleteCheckingModel.exclude_deleted(query).count() == 0
+
+    instance.soft_undelete()
+    session.commit()
+    instance.assert_is_not_deleted()
+    with pytest.raises(ValueError):
+        instance.assert_is_deleted()
+
+    assert SoftDeleteCheckingModel.filter_deleted(query).count() == 0
+    assert SoftDeleteCheckingModel.exclude_deleted(query).count() == 1
+
+    session.delete(instance)
+    with pytest.raises(HTTPConflict):
+        session.commit()
+
