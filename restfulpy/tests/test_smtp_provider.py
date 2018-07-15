e1a5037e552adb50fca26bae6f19c2df4d1b0c65
pylover <vahid.mardani@gmail.com>
2018-07-16 00:06:49 +0430

    The smtp provider has been successfuly migrated to pytest system, #191

---
+++ restfulpy/tests/test_smtp_provider.py
@@ -0,0 +1,62 @@
+
+import io
+import unittest
+from os.path import dirname, abspath, join
+
+from nanohttp import settings, configure
+from restfulpy.messaging.providers import SmtpProvider
+from restfulpy.mockup import mockup_smtp_server
+
+HERE = abspath(dirname(__file__))
+
+
+def test_smtp_provider():
+    configure(init_value=f'''
+        smtp:
+          host: smtp.example.com
+          port: 587
+          username: user@example.com
+          password: password
+          local_hostname: localhost
+          tls: false
+          auth: false
+          ssl: false
+        messaging:
+          mako_modules_directory: {join(HERE, '../../data', 'mako_modules')}
+          template_dirs:
+            - {join(HERE, 'templates')}
+        ''',
+        force=True
+    )
+
+    with mockup_smtp_server() as (server, bind):
+        settings.smtp.host = bind[0]
+        settings.smtp.port = bind[1]
+
+        # Without templates
+        SmtpProvider().send(
+            'test@example.com',
+            'test@example.com',
+            'Simple test body',
+            cc='test@example.com',
+            bcc='test@example.com'
+        )
+
+        # With template
+        SmtpProvider().send(
+            'test@example.com',
+            'test@example.com',
+            {},
+            template_filename='test-email-template.mako'
+        )
+
+        # With attachments
+        attachment = io.BytesIO(b'This is test attachment file')
+        attachment.name = 'path/to/file.txt'
+        SmtpProvider().send(
+            'test@example.com',
+            'test@example.com',
+            'email body with Attachment',
+            attachments=[attachment]
+        )
+
