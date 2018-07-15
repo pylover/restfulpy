1f93d6eac987a410b6bbd93e6868e564f4570d74
mohammad <mohammadsheikhian70@gmail.com>
2018-07-15 16:31:37 +0430

    Passing the message models test case, #191

---
+++ restfulpy/tests/test_messaging_models.py
@@ -0,0 +1,56 @@
+from nanohttp import settings
+
+from restfulpy.orm import Field, FakeJson
+from restfulpy.messaging import BaseEmail, create_messenger
+
+
+class Welcome(BaseEmail):
+    __tablename__ = 'welcome'
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'welcome'
+    }
+
+    body = Field(FakeJson, json='body')
+
+    @property
+    def email_body(self):
+        return self.body
+
+    @property
+    def template_filename(self):
+        return None
+
+
+def test_messaging_model(db):
+    __configuration__ = '''
+    messaging:
+      default_sender: test@example.com
+      default_messenger: restfulpy.mockup.MockupMessenger
+    '''
+
+    settings.merge(__configuration__)
+    session = db()
+
+    mockup_messenger = create_messenger()
+
+    # noinspection PyArgumentList
+    message = Welcome(
+        to='test@example.com',
+        subject='Test Subject',
+        body={'msg': 'Hello'}
+    )
+
+    session.add(message)
+    session.commit()
+
+    message.do_({})
+
+    # noinspection PyUnresolvedReferences
+
+    assert mockup_messenger.last_message == {
+        'body': {'msg': 'Hello'},
+        'subject': 'Test Subject',
+        'to': 'test@example.com'
+    }
+
