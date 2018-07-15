5153acc4764cb4c306b0535795ab2785471fa092
pylover <vahid.mardani@gmail.com>
2018-07-15 23:57:29 +0430

    The test_impersonation has been migrated to pytest and suucessfully passed #191

--- restfulpy/tests/_test_impersonation.py
+++ restfulpy/tests/_test_impersonation.py
@@ -1,20 +1,10 @@
-import sys
-import unittest
-
 from nanohttp.contexts import context, Context
 
 from restfulpy.principal import ImpersonateAs, DummyIdentity
 
 
-class ImpersonationTest(unittest.TestCase):
-    def setUp(self):
-        self.http_context = Context({})
-        self.http_context.__enter__()
-
-    def tearDown(self):
-        self.http_context.__exit__(*sys.exc_info())
-
-    def test_impersonation(self):
+def test_impersonation():
+    with Context({}) as ctx:
         role1 = 'role-1'
         role2 = 'role-2'
         role3 = 'role-3'
@@ -23,24 +13,21 @@ class ImpersonationTest(unittest.TestCase):
         # Simple test
         role_1_principal = DummyIdentity(role1)
         with ImpersonateAs(role_1_principal):
-            self.assertEqual(context.identity, role_1_principal)
-            self.assertTrue(context.identity.is_in_roles(role1))
+            assert context.identity == role_1_principal
+            assert context.identity.is_in_roles(role1)
 
             # Now we change the role
             role_2_principal = DummyIdentity(role2)
             with ImpersonateAs(role_2_principal):
-                self.assertFalse(context.identity.is_in_roles(role1))
-                self.assertTrue(context.identity.is_in_roles(role2))
+                assert not context.identity.is_in_roles(role1)
+                assert context.identity.is_in_roles(role2)
 
         # Multiple roles
         role_3_4_principal = DummyIdentity(role3, role4)
         with ImpersonateAs(role_3_4_principal):
-            self.assertTrue(context.identity.is_in_roles(role3))
-            self.assertTrue(context.identity.is_in_roles(role4))
+            assert context.identity.is_in_roles(role3)
+            assert context.identity.is_in_roles(role4)
 
-            self.assertFalse(context.identity.is_in_roles(role1))
-            self.assertFalse(context.identity.is_in_roles(role2))
+            assert not context.identity.is_in_roles(role1)
+            assert not context.identity.is_in_roles(role2)
 
-
-if __name__ == '__main__':  # pragma: no cover
-    unittest.main()
