157306825a445dc6e8930853b54c4d6cf91e033a
pylover <vahid.mardani@gmail.com>
2019-06-04 11:34:14 +0430

    Rewrite test appcli jwt token, #220

---
+++ restfulpy/tests/test_appcli_jwt.py
@@ -0,0 +1,37 @@
+import json
+import base64
+
+import pytest
+from bddcli import Given, given, when, stdout, stderr, Application, status
+from nanohttp import settings
+
+from restfulpy import Application as RestfulpyApplication
+
+
+foo = RestfulpyApplication(name='jwt')
+
+
+def foo_main():
+    return foo.cli_main()
+
+
+app = Application('foo', 'restfulpy.tests.test_appcli_jwt:foo_main')
+
+
+def test_jwt():
+    with Given(app, 'jwt create'):
+        assert stderr == ''
+        assert status == 0
+        assert len(stdout) > 10
+
+        when(given + '\'{"foo": 1}\'')
+        assert stderr == ''
+        assert status == 0
+        header, payload, signature = stdout.encode().split(b'.')
+        payload = base64.urlsafe_b64decode(payload)
+        assert json.loads(payload) == {'foo': 1}
+
+
+if __name__ == '__main__':  # pragma: no cover
+    foo.cli_main(['jwt', 'create', '{"foo": 1}'])
+
