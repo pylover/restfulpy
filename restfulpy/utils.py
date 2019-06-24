25b3d483ab11e1bbc0f7394b14f7bf888b03e193
Mohammad Sheikhian <mohammadsheikhian70@gmail.com>
2019-06-24 14:06:51 +0430

    Revert "Purge the deprecated decorator from the utils module (#330)"
    This reverts commit 8d13b64c92d3b65d48a5d3570e350dd2d64223e2.

--- restfulpy/utils.py
+++ restfulpy/utils.py
@@ -49,6 +49,29 @@ def construct_class_by_name(name, *args, **kwargs):
     return getattr(module, class_name)(*args, **kwargs)
 
 
+def deprecated(func):
+    """
+    This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emmitted
+    when the function is used.
+    """
+
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # Turn off filter
+        warnings.warn(
+            'Call to deprecated function %s.' % func.__name__,
+            category=DeprecationWarning,
+            stacklevel=2
+        )
+        warnings.simplefilter('default', DeprecationWarning)  # Reset filter
+        return func(*args, **kwargs)
+
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+    new_func.__dict__.update(func.__dict__)
+    return new_func
+
+
 def to_camel_case(text):
     return re.sub(r'(_\w)', lambda x: x.group(1)[1:].upper(), text)
 
