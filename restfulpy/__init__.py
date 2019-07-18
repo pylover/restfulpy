a7e1bec1a9497fe5e740451f03c8e78be6d7988e
pylover <vahid.mardani@gmail.com>
2019-07-18 23:31:09 +0430

    v3.3.0a4

--- restfulpy/__init__.py
+++ restfulpy/__init__.py
@@ -6,5 +6,5 @@ from .application import Application
 warnings.filterwarnings('ignore', message='Unknown REQUEST_METHOD')
 
 # TODO: unused imports
-__version__ = '3.2.2a3'
+__version__ = '3.3.0a4'
 
